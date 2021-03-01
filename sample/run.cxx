603a925796983a1df809d35074f0354fc86dd557
Pratik Chowdhury <pratikc@live.co.uk>
2021-03-01 14:00:47 +0530

    [Sample] Added

---
+++ sample/run.cxx
@@ -0,0 +1,45 @@
+#include <type_traits>
+#include <scoped_exit.hxx>
+#include <iostream>
+
+void no_throw ()
+{
+   // Note that as the Function provided here is noexcept
+   // So will our destructor
+
+   // Note that throwing in a nothrow lambda
+   // Will lead to noexcept semantics
+   pc::finally nothrow {[]() noexcept{
+      std::cout << "Perform cleanup here\n";
+   }};
+   std::cout << "Function Started\n";
+   std::cout << "Function Ended\n";
+}
+
+void throw_but_catch ()
+{
+   try{
+   // GSL destructor is noexcept
+   // But our cleanup may throw an exception
+   // Which we might catch later
+
+   pc::finally throws {[]() {
+      std::cout << "During Cleanup, we had to throw\n";
+      throw std::exception("Sorry. Something during Cleanup errored out");
+   }};
+
+   std::cout << "Block Started\n";
+   std::cout << "Block Ended\n";
+   // Destructor called here
+   }
+   catch (std::exception ex)
+   {
+      std::cout << "Exception caught : " << ex.what() << "\n";
+   }
+}
+int main ()
+{
+   no_throw();
+   // GSL would not have been able to handle it
+   throw_but_catch();
+}
